b7fc81923aed2c32e29c8006029e7c94f5836e21
thomas <thomas.baullard@gmail.com>
2024-12-05 09:41:30 +0100

    ajout de la branche POO

---
+++ app.js
@@ -0,0 +1,19 @@
+class Player {
+    constructor(name, id){
+        this.name = name;
+        this.id = id;
+    }
+}
+class Griffin{
+    constructor(){
+        super(this.name
+            
+        );
+    }
+}
+class Witch{
+
+}
+class Game {
+
+}
